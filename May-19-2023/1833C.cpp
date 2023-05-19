d0816ea1f11289a1b03fc1efe381f8ddd8613dda
Glastr1er <85101232+Glastr1er@users.noreply.github.com>
2023-05-19 15:47:43 -0700

    Create 1833C.cpp

---
+++ May-19-2023/1833C.cpp
@@ -0,0 +1,87 @@
+/*
+Author: atharvd
+Date: May/19/2023
+Problem: 1833C
+*/
+
+#include<iostream>
+#include<string>
+#include<map>
+#include<queue>
+#include<set>
+#include<stack>
+#include<vector>
+#include<algorithm>
+#include<functional>
+#include<numeric>
+#include<cmath>
+
+using namespace std;
+
+#define ain(a) for(int i=0;i<a.size();i++) cin >> a[i]
+#define aout(a) for(int i=0;i<a.size();i++) { if(i > 0) cout << " "; cout << a[i]; }; cout << endl
+#define couty cout << "YES\n"
+#define coutn cout << "NO\n";
+
+#define pb push_back
+#define eb emplace_back
+#define all(a) a.begin(),a.end()
+
+#define nl cout << endl
+
+#ifdef LOCAL_JUDGE
+#include "local.hpp"
+#else
+template <typename Head, typename... Tail> void dout(Head&& h, Tail&&... t) {}
+#endif
+
+typedef long long ll;
+typedef pair<int,int> pii; typedef pair<ll,int> pli; typedef tuple<ll,int,int> tllii;
+typedef vector<int> vi; typedef vector<bool> vb; typedef vector<ll> vll; typedef vector<vector<int> > vvi;
+
+ll inf = 1e18 + 1;
+
+void solve() {
+    int n;
+    cin >> n;
+    vi a(n);
+    ain(a);
+    int se = 1e9;
+    int so = 1e9;
+    for(int i = 0; i < n; i++){
+        if(a[i] % 2 == 0){
+            se = min(se, a[i]);
+        }
+        else{
+            so = min(so, a[i]);
+        }
+    }
+    if(se == 1e9 || so == 1e9){
+        cout << "YES\n";
+        return;
+    }
+    for(int i = 0; i < n; i++){
+        if(a[i] % 2 == 0){
+            if(a[i] <= so){
+                cout << "NO\n";
+                return;
+            }
+        }
+    }
+    cout << "YES\n";
+}
+
+int main(int argc,char *argv[]){
+#ifdef LOCAL_JUDGE
+	freopen(argc > 1 ? argv[1] : "in.txt", "r", stdin);
+#endif
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);cout.tie(NULL);
+ 
+	int t = 1;
+	cin >> t;
+	for(int i=1;i<=t;i++) {
+		dout("***",i);
+		solve();
+	}
+}
