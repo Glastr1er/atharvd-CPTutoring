077390c35cb5b5f6ebdbd634af01d7757a1a9e7e
Glastr1er <85101232+Glastr1er@users.noreply.github.com>
2023-05-21 08:44:40 -0700

    Create 429B.cpp

---
+++ May-21-2023/429B.cpp
@@ -0,0 +1,80 @@
+/*
+Author: atharvd
+Date: May/21/2023
+Problem: 492B
+*/
+
+#include<iostream>
+#include<string>
+#include<map>
+#include<queue>
+#include<set>
+#include<stack>
+#include<vector>
+#include<algorithm>
+#include<functional>
+#include<numeric>
+#include<cmath>
+
+using namespace std;
+
+#define ain(a) for(int i=0;i<a.size();i++) cin >> a[i]
+#define aout(a) for(int i=0;i<a.size();i++) { if(i > 0) cout << " "; cout << a[i]; }; cout << endl
+#define couty cout << "YES\n"
+#define coutn cout << "NO\n";
+
+#define pb push_back
+#define eb emplace_back
+#define all(a) a.begin(),a.end()
+
+#define nl cout << endl
+
+#ifdef LOCAL_JUDGE
+#include "local.hpp"
+#else
+template <typename Head, typename... Tail> void dout(Head&& h, Tail&&... t) {}
+#endif
+
+typedef long long ll;
+typedef pair<int,int> pii; typedef pair<ll,int> pli; typedef tuple<ll,int,int> tllii;
+typedef vector<int> vi; typedef vector<bool> vb; typedef vector<ll> vll; typedef vector<vector<int> > vvi;
+
+ll inf = 1e18 + 1;
+
+void solve() {
+    int n, l;
+    cin >> n >> l;
+    multiset<int> lanterns;
+    for(int i = 0; i < n; i++){
+        int x;
+        cin >> x;
+        lanterns.insert(x);
+    }
+    int dist = 0;
+    for(auto e = lanterns.begin(); e != --lanterns.end(); e++){
+        auto x = e;
+        x++;
+        dist = max(dist, *x - *e);
+    }
+    dist = max(dist, *lanterns.begin() * 2);
+    dist = max(dist, 2 * (l - *--lanterns.end()));
+    double c = dist;
+    c/=2;
+    cout << fixed;
+    cout << c << endl;
+}
+
+int main(int argc,char *argv[]){
+#ifdef LOCAL_JUDGE
+	freopen(argc > 1 ? argv[1] : "in.txt", "r", stdin);
+#endif
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);cout.tie(NULL);
+ 
+	int t = 1;
+	//cin >> t;
+	for(int i=1;i<=t;i++) {
+		dout("***",i);
+		solve();
+	}
+}
