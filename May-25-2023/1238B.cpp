15eaceaa561e21914617aff76051d91d1ae02934
Glastr1er <85101232+Glastr1er@users.noreply.github.com>
2023-05-25 22:35:47 -0700

    Create 1238B.cpp

---
+++ May-25-2023/1238B.cpp
@@ -0,0 +1,69 @@
+/*
+Author: atharvd
+Date: May/25/2023
+Problem: 1238B
+*/
+
+#include<iostream>
+#include<string>
+#include<map>
+#include<queue>
+#include<set>
+#include<stack>
+#include<vector>
+#include<algorithm>
+#include<functional>
+#include<numeric>
+#include<cmath>
+
+using namespace std;
+
+#define ain(a) for(int i=0;i<a.size();i++) cin >> a[i]
+#define aout(a) for(int i=0;i<a.size();i++) { if(i > 0) cout << " "; cout << a[i]; }; cout << endl
+#define couty cout << "YES\n"
+#define coutn cout << "NO\n";
+
+#define pb push_back
+#define eb emplace_back
+#define all(a) a.begin(),a.end()
+
+#define nl cout << endl
+
+#ifdef LOCAL_JUDGE
+#include "local.hpp"
+#else
+template <typename Head, typename... Tail> void dout(Head&& h, Tail&&... t) {}
+#endif
+
+typedef long long ll;
+typedef pair<int,int> pii; typedef pair<ll,int> pli; typedef tuple<ll,int,int> tllii;
+typedef vector<int> vi; typedef vector<bool> vb; typedef vector<ll> vll; typedef vector<vector<int> > vvi;
+
+ll inf = 1e18 + 1;
+
+void solve() {
+    int n, k;
+    cin >> n >> k;
+    vi pref(n + 1);
+    for(int i = 1; i <= n; i++){
+        pref[i] = pref[i - 1] + i;
+    }
+    int x = n - (lower_bound(all(pref), k) - pref.begin()) - 1;
+    int y = (lower_bound(all(pref), k) - pref.begin()) * ((lower_bound(all(pref), k) - pref.begin()) - 1) / 2;
+    cout << x << " " << y << endl;
+}
+
+int main(int argc,char *argv[]){
+#ifdef LOCAL_JUDGE
+	freopen(argc > 1 ? argv[1] : "in.txt", "r", stdin);
+#endif
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);cout.tie(NULL);
+ 
+	int t = 1;
+	cin >> t;
+	for(int i=1;i<=t;i++) {
+		dout("***",i);
+		solve();
+	}
+}
